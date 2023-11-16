a553cd0609b8f3221e91c1b79c94250afacb44b2
Angelica <Angelica137@users.noreply.github.com>
2023-11-16 17:47:04 +0000

    test intro statement, red

---
+++ test_game.py
@@ -0,0 +1,2 @@
+def test_intro_returns():
+	assert intro() == "You find yourself standing in an open field, filled with grass and yellow wildflowers.Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village...."
