b99cb51e180490332b946eace2de86b65eeda21b
Angelica <Angelica137@users.noreply.github.com>
2023-11-16 18:02:36 +0000

    print intro statement

--- test_game.py
+++ test_game.py
@@ -1,2 +1,6 @@
-def test_intro_returns():
-	assert intro() == "You find yourself standing in an open field, filled with grass and yellow wildflowers.Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village...."
+from game import intro
+
+def test_intro(capsys):
+	intro()
+	captured = capsys.readouterr()
+	assert captured.out.strip() == "You find yourself standing in an open field, filled with grass and yellow wildflowers. Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village..."
