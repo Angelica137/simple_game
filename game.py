cc1cc932fe9de11130437de4b13df06d493830e1
Angelica <Angelica137@users.noreply.github.com>
2023-11-17 16:27:29 +0000

    annotate functions

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 def get_user_input(prompt) -> str:
+    """Capture user input"""
     return input(prompt)
 
 def intro() -> str:
+    """Print intro statement and collect user input"""
     return get_user_input("Q: ")
 
 
