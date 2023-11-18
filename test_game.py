4e0610037b084b1a6d2befc82778b3a37a2e6c33
Angelica <Angelica137@users.noreply.github.com>
2023-11-18 13:39:13 +0000

    add tests for all cases of first user action knock knock

--- test_game.py
+++ test_game.py
@@ -35,27 +35,35 @@ def test_long_story():
     assert "You are in the middle of an enchanted forest where your parents left you with some bread" in output_lines[0]
     assert "After walking for a while you come across a cabin made of ginger bread and candy." in output_lines[1]
     assert "You are suspicious, but you are also tired and hungry." in output_lines[2]
-
-
-def test_first_action(monkeypatch, capfd):
-    monkeypatch.setattr('game.get_user_input', lambda _: 1)
-    with capfd.disabled():
-        result = first_action()        
-    assert result == 1
     
 
 def test_first_action_user_input_1():
+    """test user input from fisrt action is captured"""
     with patch("builtins.input", side_effect=["1"]):
         result = first_action()
     assert result == "1"
-    
 
+'''
+def test_first_action_user_input_2():
+    """test user input from fisrt action is captured value 2"""
+    with patch("builtins.input", side_effect=["2"]):
+        result = first_action()
+    assert result == "2"
+'''
 
 def test_knock_knock_path_1():
-    with patch("game.first_action", return_value="1"):
+    with patch("game.first_action", side_effect="1"):
         result = knock_knock()
     assert result == "A young man opens the door."
 
 
+def test_knock_knock_path_2():
+    with patch("game.first_action", side_effect="2"):
+        result = knock_knock()
+    assert result == "You go into stealth mode and move around the property towards the neares candy bush."
 
 
+def test_knock_knock_path_other():
+    with patch("game.first_action", side_effect=["4", "2"]):
+        result = knock_knock()
+    assert result == "You go into stealth mode and move around the property towards the neares candy bush."
