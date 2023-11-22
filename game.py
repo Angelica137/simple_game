0c2ab53eb695c125993057536e6b59de42c906f8
Angelica <Angelica137@users.noreply.github.com>
2023-11-22 14:52:05 +0000

    garden_picking() accepts user input, green, statements now red, this added 2 statements

--- game.py
+++ game.py
@@ -58,6 +58,7 @@ def garden_picking() -> str:
     print("Do not eat that!” a little voice says")
     pause()
     print("You look around and see nothing, what do you do?")
+    return get_user_input("Enter 1 to knock on the door.\nEnter 2 to pick whatever you can grab.\n")
     
     
 
