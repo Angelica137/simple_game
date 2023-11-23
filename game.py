6ba8b4649646047f81ed902639531ceb0fb26c49
Angelica <Angelica137@users.noreply.github.com>
2023-11-23 14:39:01 +0000

    fix typos

--- game.py
+++ game.py
@@ -52,6 +52,7 @@ def cabin_knock() -> str:
 
 
 def garden_picking() -> str:
+    """Lays the story for what happens at the garden and asks for input"""
     print("You take care to hide amongst the bushes and try to stay aware of your surroundings and the cabin.")
     pause()
     print("After a while of nothing happening you decide to start picking up as much candy as you can and fit it in your pockets.")
@@ -60,39 +61,41 @@ def garden_picking() -> str:
     pause()
     print("You decide to have some right there and then, after all, your pockets are getting full and heavy and you need to keep your sugar levels up.")
     pause()
-    print("Do not eat that!” a little voice says.")
+    print("“Do not eat that!” a little voice says.")
     pause()
     print("You look around and see a little fairy, what do you do?")
-    return garden_picking_input()
+    return marshmallows()
 
 
 def garden_picking_input():
-    return get_user_input("1. You shrug and keep on eating the marshmallows.\n2. Start talking to the fairy\n")
-
-
-def marshmallows() -> str:
-    while True:
-        path = garden_picking_input()
-        if path == "1":
-            return "You start crawling backwards, slowly."
-        if path == "2":
-            return "Hello little fairy! I did not mean to steal these from you."
+    """Gives the user choices for the garden action"""
+    return get_user_input("Enter 1 to shrug and keep on eating the marshmallows.\nEnter 2 to start talking to the fairy\n")
 
 
 def fairy_outcomes_lose() -> str:
-    print("You start crawling backwards, slowly.")
+    print("You shrug and keep on eating. She is so tiny, and you are so hungry.")
     pause()
     print("“No wait!” you hear the little fairy scream, and then, it all goes black. You are dead :(")
     pause()
-    return "GAME OVER."
+    return "GAME OVER.\nDo you want to play again? y/n"
+
+
+def marshmallows() -> str:
+    """Outcomes from action choices at the garden"""
+    while True:
+        path = garden_picking_input()
+        if path == "1":
+            return fairy_outcomes_lose()
+        if path == "2":
+            return fairy_outcomes_win()
 
 
 def fairy_outcomes_win() -> str:
     print("Hello little fairy! I did not mean to steal these from you.")
     pause()
-    print("“you are not stealing from me. you are stealing from him!” She says angrily while pointing her tiny finger behind you.")
+    print("“You are not stealing from me. you are stealing from him!” She says angrily while pointing her tiny finger behind you.")
     pause()
-    print("You turn around and see a large oar ready to hit you with a huge club.")
+    print("You turn around and see a large ogre ready to hit you with a huge club.")
     pause()
     print("You dodge the ogre and see him smash the little fairy. You grab your pockets full of candy and run for your life!")
     pause()
@@ -102,11 +105,12 @@ def fairy_outcomes_win() -> str:
     pause()
     print("“Ogre!” finally you manage the words as the ogre makes it out of the forest finally catching up.")
     pause()
-    print("The lumberjacks see him and launch an attack on the ogre leaving you on the ground with some candy still i your pockets.")
-    return "YOU WIN!"
+    print("The lumberjacks see him and launch an attack on the ogre leaving you on the ground with some candy still in your pockets.")
+    return "YOU WIN!\nDo you want to play again? y/n"
 
-'''
-print(intro())
-print(first_action())
-print(forest_cabin())
-'''
+
+#print(intro())
+#print(first_action())
+#print(forest_cabin())
+#print(marshmallows())
+
