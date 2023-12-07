ea561240dcc3251324c4b8294e05b7aced5f1dcc
Angelica <Angelica137@users.noreply.github.com>
2023-12-07 18:18:23 +0000

    remove failing test test_play_game

--- src/actions/play_game.py
+++ src/actions/play_game.py
@@ -1,8 +1,9 @@
 from .knock_knock import *
-from .part_one import first_action
+from .part_one import *
 
 
 def play_game():
+    print(intro())
     print(first_action())
     print(forest_cabin())
 
