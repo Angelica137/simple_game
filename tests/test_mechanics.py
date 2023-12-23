4dd2c1428b29f2efc4c19dbd480d0beb525a7407
Angelica <Angelica137@users.noreply.github.com>
2023-12-23 21:40:53 +0000

    action pycodestyle comms to test_mechanics

--- tests/test_mechanics.py
+++ tests/test_mechanics.py
@@ -1,6 +1,7 @@
 from src.actions.mechanics import *
 from freezegun import freeze_time
 from unittest.mock import patch, call
+import pytest
 
 
 def test_pause():
@@ -23,7 +24,7 @@ def test_story_telling():
         assert result == "YOU WIN!"
 
 
-def test_choices():
+def test_choices_path_1():
     """tests the user is given choices and moves the game"""
     mock_options = {"prompt": "enter 1 or 2",
                     "1": lambda: "1 is valid",
@@ -31,3 +32,55 @@ def test_choices():
     with patch("builtins.input", side_effect=["1"]):
         result = choices(mock_options)
     assert result == "1 is valid"
+
+
+def test_choices_path_2():
+    """tests the user is given choices and moves the game"""
+    mock_options = {"prompt": "enter 1 or 2",
+                    "1": lambda: "1 is valid",
+                    "2": lambda: "is valid too"}
+    with patch("builtins.input", side_effect=["2"]):
+        result = choices(mock_options)
+    assert result == "is valid too"
+
+
+'''
+class InvalidChoiceError(Exception):
+    pass
+
+
+def test_invalid_input(capsys, monkeypatch):
+    """Tests the 'else' statement in the choices function"""
+    # Mock the input function to return an invalid choice
+    monkeypatch.setattr('builtins.input', lambda _: '3')
+
+    mock_options = {"prompt": "Enter 1 or 2",
+                    "1": lambda: "Option 1 chosen",
+                    "2": lambda: "Option 2 chosen"}
+
+    # Monkeypatch the first_choices function to raise InvalidChoiceError after
+    # one iteration
+    with monkeypatch.context() as m:
+        count = 0
+
+        def custom_get_user_input(prompt="Enter your choice: ", sentinel=None):
+            nonlocal count
+            count += 1
+            if count > 1:
+                raise InvalidChoiceError("Invalid choice")
+            return '3'
+
+        m.setattr('src.actions.mechanics.choices',
+                  custom_get_user_input)
+
+    # Call choices
+    with pytest.raises(InvalidChoiceError, match="Invalid choice"):
+        choices(mock_options)
+
+    # Capture the printed output
+    captured_output = capsys.readouterr()
+
+    # Check the expected output
+    expected_output = "Oops! invalid_choice is not an option.\n"
+    assert captured_output.out == expected_output
+'''
